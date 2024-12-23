59792a79f77750deba674076cb33e3c9b0ec8e4d
LouisDeOliveira <louisdeoliveira@gmail.com>
2024-12-23 17:23:58 +0100

    call functions, send binary data

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include "crow.h"
+#include <crow/json.h>
+#include <cstdint>
 #include <fstream>
+#include <ios>
 #include <sstream>
+#include <string>
+#include <vector>
 
 // Helper to serve static files
 std::string readFile(const std::string &path)
@@ -13,6 +18,27 @@ std::string readFile(const std::string &path)
     std::ostringstream contents;
     contents << file.rdbuf();
     return contents.str();
+}
+
+void functionA()
+{
+    std::cout << "Function A called" << std::endl;
+}
+
+void functionB()
+{
+    std::cout << "Function B called" << std::endl;
+}
+
+std::vector<uint8_t> readBinaryFile(const std::string &path)
+{
+    std::ifstream ifs(path, std::ios::binary | std::ios::in);
+    if (!ifs)
+    {
+        std::cerr << "Failed to open file: " << path << std::endl;
+        return {};
+    }
+    return std::vector<uint8_t>(std::istreambuf_iterator<char>(ifs), std::istreambuf_iterator<char>());
 }
 
 int main()
@@ -33,6 +59,44 @@ int main()
             {
                 auto data = req.body; // Process incoming data
                 return crow::response("Received: " + data + " lol");
+            });
+
+    // Route to call function A
+    CROW_ROUTE(app, "/api/functionA")
+        .methods(crow::HTTPMethod::POST)(
+            [](const crow::request &req)
+            {
+                functionA();
+                return crow::response("Function A called");
+            });
+
+    // Route to call function B
+    CROW_ROUTE(app, "/api/functionB")
+        .methods(crow::HTTPMethod::POST)(
+            [](const crow::request &req)
+            {
+                functionB();
+                return crow::response("Function B called");
+            });
+
+    // Route to request a binary file
+    CROW_ROUTE(app, "/api/binary")
+        .methods(crow::HTTPMethod::POST)(
+            [](const crow::request &req)
+            {
+                std::string data_path;
+                // read the path from the query json
+                crow::json::rvalue json = crow::json::load(req.body);
+                if (json.has("path"))
+                {
+                    data_path = json["path"].s();
+                }
+                else
+                {
+                    return crow::response("No path provided");
+                }
+                auto data = readBinaryFile(data_path);
+                return crow::response(std::string(data.begin(), data.end()));
             });
 
     // Start the server on a single port
--- src/static/index.js
+++ src/static/index.js
@@ -1,9 +1,54 @@
-console.log('Hello, world!');
+console.log("Hello, world!");
+
+var count = 0;
 
 function sendMessage() {
-    fetch("/api/click", { method: "POST" , headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ message: "Hello, world!" }) })
+    count++;
+    fetch("/api/click", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ message: "Hello, world!, count = " + count }),
+    })
         .then((res) => res.text())
         .then((data) => {
             document.getElementById("response").innerText = data;
+        });
+}
+
+function callFunctionA() {
+    fetch("/api/functionA", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ message: "Hello, world" }),
+    })
+        .then((res) => res.text())
+        .then((data) => {
+            document.getElementById("response").innerText = data;
+        });
+}
+
+function callFunctionB() {
+    fetch("/api/functionB", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ message: "Hello, world" }),
+    })
+        .then((res) => res.text())
+        .then((data) => {
+            document.getElementById("response").innerText = data;
+        });
+}
+
+function requestData() {
+    fetch("/api/binary", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ path: "../assets/gromit.jpg" }),
+    })
+        .then((res) => res.blob())
+        .then((data) => {
+            var img = document.createElement("img");
+            img.src = URL.createObjectURL(data);
+            document.getElementById("response").appendChild(img);
         });
 }
